b421a5f690c40f7c3df2c4ec760311fd9cea422c
Brigham <brig.johnson@gmail.com>
2022-04-26 22:48:43 -0400

    commit tree 70c17d252ffaf991c35c37812ec0f13cdc5b7af8

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetToGo){
+  if (feetToGo <= 400) {
+    return 'This one is on me!';
+  }
+  else if (feetToGo > 400 && feetToGo <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (feetToGo > 2000 && feetToGo <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (feetToGo > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  return destinationCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(levelOfCharm){
+  if (levelOfCharm === 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (levelOfCharm === 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
